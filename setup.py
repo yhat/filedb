16de22b15b104ef03bc16d1c5351b5aff3046c6a
Greg <greg@yhatHQ.com>
2014-02-04 10:34:44 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='filedb',
-    version='0.1',
+    version='0.2.1',
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/filedb",

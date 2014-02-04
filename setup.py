13531b1443fa4235cc1c4bf289b3f74f125d36d0
Greg <greg@yhatHQ.com>
2014-02-04 10:45:19 -0500

    w/ rst

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages
 
 setup(
     name='filedb',
-    version='0.2.1',
+    version='0.2.2',
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/filedb",
     packages=find_packages(),
     description="Create a file that pretends to be a database query",
     license='BSD',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     scripts=['bin/filedb'],
     install_requires=[
         "docopt==0.6.1",
